14ba59b4904bc1d4b26f2edb212f5332b6ae96f0
Anas Rchid <rchid.anas@gmail.com>
2017-01-23 14:47:57 +0000

    ADD: `struct remote`, `init_remote()`

--- main.c
+++ main.c
@@ -8,33 +8,76 @@
 
 /* you have oast with this library.. 
  * get it done this time! */
-#include <errno.h>		/* ensure you understand this library this time.. */
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/socket.h>
+# include <errno.h>		/* ensure you understand this library this time.. */
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/socket.h>
 
 /* Bluez bluetooth libraries */
-#include <bluetooth/bluetooth.h>
-#include <bluetooth/hci.h>
-#include <bluetooth/hci_lib.h>
-#include <bluetooth/rfcomm.h>
+# include <bluetooth/bluetooth.h>
+# include <bluetooth/hci.h>
+# include <bluetooth/hci_lib.h>
+# include <bluetooth/rfcomm.h>
 
-#define MAX_RESPONSES 255	/* pow(2, 8) */
+# define MAX_RESPONSES 255	/* pow(2, 8) */
+# define NAME_MAX_SIZE 255
+# define ADDRESS_MAX_SIZE 20
+struct remote{
+  int device_id, socket;
+  char name[NAME_MAX_SIZE],
+    addr[ADDRESS_MAX_SIZE];
+};
 
+struct remote init_remote(void);
 int
 main(int argc, char **argv)
 {
-  int device_id;
-  int socket;
+
+  struct remote *root = init_remote();
+
+  printf("%s %s\n", root->addr, root->name);
+  
+  return EXIT_SUCCESS;
+}
+
+struct remote
+init_remote(void)
+{
+  int i;
+  struct remote *proto = (struct remote *) malloc(sizeof(struct remote));
 
   /* just in case either device_id or socket
    * had issues while initializing.. 
    */
-  if((device_id = hci_get_route(NULL)) < 0 ||
-     (socket = hci_open_dev(DeviceId)) < 0)
+  if((proto->device_id = hci_get_route(NULL)) < 0 ||
+     (proto->socket = hci_open_dev(DeviceId)) < 0)
     exit(1);
 
-  
-  return EXIT_SUCCESS;
+  inquiry_info *info = (inquiry_info *) malloc(MAX_RESPONSES * sizeof(inquiry_info));
+
+  /* read documentaion.. */
+  int responses = hci_inquiry(proto->device_id,
+			      8,
+			      MAZ_RESPONSES,
+			      NULL,
+			      &info,
+			      IREQ_CACHE_FLUSH);
+
+  for(i = 0; i < responses; ++i) {
+    memset(proto->name, o, sizeof(proto->name));
+    
+    /* Umm, really? WHAT'S ba in ba2tr? */
+    ba2str(&(info)->bdaddr, proto->addr);
+    
+    /* in case the device has no name, set name to UNKNOWN */
+    if(hci_read_remote_name(proto->socket, &(info + i)->bdaddr,
+			    sizeof(proto->name), proto->name, 0))
+      strcpy(proto->name, "UNKNOWN");
+    /* printf("%s %s\n", proto->addr, proto->name); */
+  }
+
+  free(info);
+
+  return proto;
 }
